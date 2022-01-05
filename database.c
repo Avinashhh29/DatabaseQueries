bb66680e37942f86af7327e9f48cbfcd90b417f8
Avinash <73740762+Avinashhh29@users.noreply.github.com>
2022-01-05 18:26:49 +0530

    SyntaxCheck function

--- database.c
+++ database.c
@@ -6,14 +6,15 @@ void insert(char words[][MAX] , int count){
 	FILE *fp;
 	if(fp = fopen(words[2] , "r")){
 		fp = fopen(words[2] , "a");
+		fprintf(fp,"\n");
 		for(int i=3; i<count-1 ; i++){
 			fprintf(fp ,"%s\t\t" ,words[i]);
 		}
 		fprintf(fp,"\n");
+		printf("Inserting values into tables..\n");
 		
 	}
-	else{
-		
+	else{	
 		printf("Table does not exists\n");
 		
 	}
@@ -25,10 +26,11 @@ void create(char words[][MAX] , int count){
 	}
 	else{
 		fp = fopen(words[2] , "w");
-		for(int i=3; i<count-1 ; i++){
+		for(int i=3; i<count-1 ; i++)
+		{
 			fprintf(fp ,"%s(%s)\t" ,words[i],words[i++]);
-			fputs("\n" , fp);
 		}
+		printf("Creating  Table..\n");
 	}
 
 }
@@ -42,13 +44,14 @@ void databasefun(char words[][MAX] , int size){
 		}
 	}
 }
+
 void keywords(char str[] , int size){
 	char words[MAX][MAX];
 	int i,j,k;
 	j=0;k=0;
 	int count = 0;
 	for(i = 0 ; i < size; i++ ){
-		if(str[i] == ' ' || str[i] == '(' || str[i] == ')' || str[i] == ',' || str[i] == ';' ||  str[i] == '\n'){
+		if(str[i] == ' ' || str[i] == '(' || str[i] == ')' || str[i] == ',' || str[i] == ';'){
 				words[j][k] = '\0';
 				j++;
 				k = 0;
@@ -59,36 +62,64 @@ void keywords(char str[] , int size){
 			k++;
 		}
 	}
-
-	databasefun(words,count);	
+	databasefun(words,count);
+		
 }
-char *inputQuery(FILE *fp,int size){
-	char *str;
-	int len = 0;
-	int ch;
-	str = realloc(NULL,sizeof(*str)*size);
-	if(!str)
-		return str;
-	while(EOF != (ch = fgetc(fp)) && ch != '\n'){
-		str[len++]=ch;
-		if(len == size){
-			str = realloc(str,sizeof(*str)*(size += 10));
-			if(!str)
-				return str;
+int len(char string[]){
+	int i;
+	for(i =0; string[i] != '\0'; i++);
+	return i;
+}
+void syntaxCheck(char *str){
+	int strlen = len(str);
+	int flag=0;
+	int j,k,i;
+	//printf("%d", strlen);
+	for(i = 0 ; i<strlen ; i++){
+		if(str[i] == '(')
+			j = i;
+		if(str[i] == ')')
+			k = i;
+	}
+	for(i = j ; i<=k ; i++){
+		if(str[i] == ',')
+			flag++;
+	}
+	if(flag == 0)
+		printf("Syntax Error!\n");
+	else
+		keywords(str,strlen);
+	
+}
+void inputQuery(FILE *fp,int size){
+	char *str = malloc(sizeof(char) * size);
+	int c;
+	int len=0;
+	while(EOF != (c = fgetc(fp)) && c != '\n'){
+		str[len++]=c;
+		if(len==size){
+			str = realloc(str, sizeof(char) * (size *= 2));
 		}
 	}
 	str[len++]='\0';
-	printf("%s\n", str);
-	//return realloc(str,sizeof(*str)*len);
-	keywords(str,len);
 	
-	
+	if(str[len-2] != ';'){
+		
+		printf("syntax Error!\n");
+	}
+		
+	else{
+		syntaxCheck(str);	
+	}
+	//printf("%s\n", str);
 }
 
-int main(){
-	char *m;
+
+int main(){	
 	printf("mysql>");
-	m = inputQuery(stdin,20);
+	inputQuery(stdin,20);	
+	//syntaxCheck(m,strlen);
+	return 0;
+
 	
-	//free(m);
 }
